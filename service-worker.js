f1efc138ce2de2adf72818950b72819190a465c1
f-arruza <f.arruza@uniandes.edu.co>
2018-08-14 19:28:22 -0500

    Guardar AppShell en cache haciendo uso de Service Worker (PASO 4).

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,52 @@
 // Service Worker
+var cacheName = 'rarpPWA-step-4';
+
+var filesToCache = [
+  '/',
+  '/index.html',
+  '/scripts/app.js',
+  '/styles/inline.css',
+  '/images/ic_add_white_24px.svg',
+  '/images/ic_refresh_white_24px.svg'
+];
+
+// Almacenar en caché los recursos del sitio
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/*
+  Actualizar la caché cada vez que cambie cualquiera de los archivos del
+  shell de la app.
+*/
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        //if (key !== cacheName && key !== dataCacheName) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+// Obtener el shell de la app desde la caché.
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
