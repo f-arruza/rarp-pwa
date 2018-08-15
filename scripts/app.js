3b0da021ddce4dc451e8f8734dbf807aed72f11d
f-arruza <f.arruza@uniandes.edu.co>
2018-08-15 15:13:59 -0500

    El almacenamiento en LocalStorage se pasa a IndexedDB (PASO 8).

--- scripts/app.js
+++ scripts/app.js
@@ -8,7 +8,11 @@
         spinner: document.querySelector('.loader'),
         cardTemplate: document.querySelector('.cardTemplate'),
         container: document.querySelector('.main'),
-        addDialog: document.querySelector('.dialog-container')
+        addDialog: document.querySelector('.dialog-container'),
+        indexedDB: window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB || window.shimIndexedDB,
+        iDBTransaction: window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction,
+        iDBKeyRange: window.IDBKeyRange || window.webkitIDBKeyRange || window.msIDBKeyRange,
+        useIndexedDB: true
     };
 
     /*****************************************************************************
@@ -120,13 +124,44 @@
      *
      ****************************************************************************/
 
-     // Save list of cities to localStorage.
+     // Save list of cities to localStorage
      app.saveSelectedTimetables = function() {
        var selectedTimetables = JSON.stringify(app.selectedTimetables);
-       localStorage.selectedTimetables = selectedTimetables;
+
+       if(!app.useIndexedDB) {
+          localStorage.selectedTimetables = selectedTimetables;
+       }
+       else {
+          var dataBase = app.indexedDB.open("rarpdb", 3);
+          dataBase.onerror = function(event) {
+              app.useIndexedDB = false;
+          };
+
+          dataBase.onsuccess = function(event) {
+              var db = event.target.result;
+
+              var tx = db.transaction(["timetables"], "readwrite");
+              // Close the db when the transaction is done
+              tx.oncomplete = function() {
+                  db.close();
+              };
+              var store = tx.objectStore("timetables");
+
+              var request = store.get("selected");
+              request.onsuccess = function(event) {
+                  if(request.result == null)
+                      store.add({id: "selected", value: selectedTimetables});
+                  else {
+                      var data = request.result;
+                      data.value = selectedTimetables;
+                      store.put(data);
+                  }
+              };
+          };
+       }
      };
 
-    app.getSchedule = function (key, label) {
+     app.getSchedule = function (key, label) {
         var url = 'https://api-ratp.pierre-grimaud.fr/v3/schedules/' + key;
 
         // Recuperar los horarios de la caché
@@ -173,6 +208,15 @@
         });
     };
 
+    app.loadDefault = function () {
+        app.updateTimetableCard(initialStationTimetable);
+        app.selectedTimetables = [
+          {key: initialStationTimetable.key, label: initialStationTimetable.label}
+        ];
+        app.saveSelectedTimetables();
+        console.log('DATOS POR DEFECTO CARGADOS!!!');
+    }
+
     /*
      * Fake timetable data that is presented when the user first uses the app,
      * or when the user has not saved any stations. See startup code for more
@@ -182,19 +226,44 @@
     var initialStationTimetable = {
         key: 'metros/1/bastille/A',
         label: 'Bastille, Direction La Défense',
-        created: '2017-07-18T17:08:42+02:00',
+        created: '2018-08-16T17:08:42+02:00',
         schedules: [
             {
-                message: '0 mn'
+                message: '30 mn'
             },
             {
-                message: '2 mn'
+                message: '40 mn'
             },
             {
-                message: '5 mn'
+                message: '50 mn'
             }
         ]
     };
+
+    // Inicialización de IndexedDB
+    if(window.indexedDB) {
+      // Open (or create) the database
+      var dataBase = app.indexedDB.open("rarpdb", 3);
+
+      dataBase.onerror = function(event) {
+         app.useIndexedDB = false;
+         console.log('Error cargando la base de datos.');
+      };
+
+      dataBase.onsuccess = function(event) {
+         console.log('Base de datos cargada correctamente.');
+      };
+
+      // Create the schema
+      dataBase.onupgradeneeded = function(event) {
+          var db = event.target.result;
+          db.createObjectStore("timetables", {keyPath: "id"});
+      };
+    }
+    else {
+      app.useIndexedDB = false;
+    }
+
 
     /*
       Inyección datos primer reload (PASO 1).
@@ -202,24 +271,53 @@
       seleccionadas, es decir, guardadas en el localStorage, por tanto se
       cargan datos por defecto.
     */
-    app.selectedTimetables = localStorage.selectedTimetables;
-    if (app.selectedTimetables) {
-      app.selectedTimetables = JSON.parse(app.selectedTimetables);
-      app.selectedTimetables.forEach(function(sch) {
-        app.getSchedule(sch.key, sch.label);
-      });
-    } else {
-      app.updateTimetableCard(initialStationTimetable);
-      app.selectedTimetables = [
-        {key: initialStationTimetable.key, label: initialStationTimetable.label}
-      ];
-      app.saveSelectedTimetables();
-      console.log('DATOS POR DEFECTO CARGADOS!!!');
+    if(!app.useIndexedDB) {
+      console.log('Carga info del localStorage.');
+      app.selectedTimetables = localStorage.selectedTimetables;
+
+      if (app.selectedTimetables) {
+          app.selectedTimetables = JSON.parse(app.selectedTimetables);
+          app.selectedTimetables.forEach(function(sch) {
+            app.getSchedule(sch.key, sch.label);
+          });
+      } else {
+          app.loadDefault();
+      }
+    }
+    else {
+      var dataBase = app.indexedDB.open("rarpdb", 3);
+      dataBase.onerror = function(event) {
+          app.useIndexedDB = false;
+      };
+
+      dataBase.onsuccess = function(event) {
+          var db = event.target.result;
+          var tx = db.transaction(["timetables"], "readwrite");
+
+          // Close the db when the transaction is done
+          tx.oncomplete = function() {
+              if(app.selectedTimetables.length == 0) {
+                app.loadDefault();
+              }
+              db.close();
+          };
+          var store = tx.objectStore("timetables");
+          var request = store.get("selected");
+
+          request.onsuccess = function(event) {
+              if(request.result != null) {
+                  app.selectedTimetables = JSON.parse(request.result.value);
+                  app.selectedTimetables.forEach(function(sch) {
+                      app.getSchedule(sch.key, sch.label);
+                  });
+              }
+          };
+      };
     }
 
     // Registro del ServiceWorker
     if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
+        navigator.serviceWorker
                .register('./service-worker.js')
                .then(function() { console.log('Service Worker Registered'); });
     }
